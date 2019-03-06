0f632808e2ad8c6f1bed9cd5cb52da2f51bcaa7a
Lucas Werner <lwerner27@gmail.com>
2019-03-06 11:34:27 -0600

    changed main app to stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+// Underscore is a convention for making objects private.
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
