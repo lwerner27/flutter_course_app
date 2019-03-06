de53d370f99c284357e9e4310b71e4bbf852966f
Lucas Werner <lwerner27@gmail.com>
2019-03-06 13:46:58 -0600

    playing around with Material Colors

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -17,9 +17,11 @@ class _ProductManagerState extends State<ProductManager> {
   List<String> _products = [];
 
   @override
-  void initState() { 
-    super.initState(); // It is recommended to move this to the beginning of the initState method.
-    _products.add(widget.startingProduct); // There is no need to use setState in initState as it runs before build.
+  void initState() {
+    super
+        .initState(); // It is recommended to move this to the beginning of the initState method.
+    _products.add(widget
+        .startingProduct); // There is no need to use setState in initState as it runs before build.
   }
 
   @override
@@ -29,6 +31,7 @@ class _ProductManagerState extends State<ProductManager> {
         Container(
           margin: EdgeInsets.all(10.0),
           child: RaisedButton(
+            color: Theme.of(context).accentColor,
             onPressed: () {
               setState(() {
                 _products.add("Advanced Food Tester");
