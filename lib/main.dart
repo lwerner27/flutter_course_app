1fa033e8fa11f0cce1781c1da18a6e6189f3b051
Lucas Werner <lwerner27@gmail.com>
2019-03-06 11:50:15 -0600

    added an onclick function for the button that adds static data to the state

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatefulWidget {
 
 // Underscore is a convention for making objects private.
 class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,18 +25,26 @@ class _MyAppState extends State<MyApp> {
             Container(
               margin: EdgeInsets.all(10.0),
               child: RaisedButton(
-                onPressed: () {},
+                onPressed: () {
+                  setState(() {
+                    _products.add("Advanced Food Tester");
+                  });
+                },
                 child: Text("Add Product"),
               ),
             ),
-            Card(
-              child: Column(
-                children: <Widget>[
-                  Image.asset('assets/food.jpg'),
-                  Text('Food Paradise'),
-                ],
-              ),
-            ),
+            Column(
+              children: _products
+                  .map((element) => Card(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/food.jpg'),
+                            Text(element),
+                          ],
+                        ),
+                      ))
+                  .toList(),
+            )
           ],
         ),
       ),
